5688368dfeec8ec9ce0339ed08b413d74bd41c6d
admin <836722685@qq.com>
2019-07-27 09:31:14 +0800

    Merge branch 'master' of https://github.com/Strvice/springboot
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/example/demo/mapper/FileInfoMapper.java
@@ -0,0 +1,17 @@
+package com.example.demo.mapper;
+
+import com.example.demo.model.FileInfo;
+
+public interface FileInfoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(FileInfo record);
+
+    int insertSelective(FileInfo record);
+
+    FileInfo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(FileInfo record);
+
+    int updateByPrimaryKey(FileInfo record);
+}
---
+++ src/main/java/com/example/demo/model/FileInfo.java
@@ -0,0 +1,63 @@
+package com.example.demo.model;
+
+public class FileInfo {
+    private Integer id;
+
+    private String path;
+
+    private String filename;
+
+    private String savename;
+
+    private String type;
+
+    private String suffix;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path == null ? null : path.trim();
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename == null ? null : filename.trim();
+    }
+
+    public String getSavename() {
+        return savename;
+    }
+
+    public void setSavename(String savename) {
+        this.savename = savename == null ? null : savename.trim();
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type == null ? null : type.trim();
+    }
+
+    public String getSuffix() {
+        return suffix;
+    }
+
+    public void setSuffix(String suffix) {
+        this.suffix = suffix == null ? null : suffix.trim();
+    }
+}

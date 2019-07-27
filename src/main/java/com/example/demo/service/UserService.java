8afa995866225fb8eca09c793d8d0c6da17e18b4
admin <836722685@qq.com>
2019-07-27 09:30:48 +0800

    Merge branch 'master' of https://github.com/Strvice/springboot
    # Conflicts:
    #	.gitignore

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,4 +1,10 @@
 package com.example.demo.service;
 
+import com.example.demo.model.FileInfo;
+
+import java.util.List;
+
 public interface UserService {
+   void addFile(FileInfo fileinfo);
+    List<FileInfo> queryFileInfo(String id);
 }
---
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -0,0 +1,18 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.model.FileInfo;
+import com.example.demo.service.UserService;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public void addFile(FileInfo fileinfo) {
+
+    }
+
+    @Override
+    public List<FileInfo> queryFileInfo(String id) {
+        return null;
+    }
+}
